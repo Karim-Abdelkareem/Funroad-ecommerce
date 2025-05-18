f7c913744e5e28b405b552ca1403ec63c9bae67b
Karim Abdelkareem <alkar33m@gmail.com>
2025-05-18 16:59:58 +0300

    03: Home Layout

---
+++ src/app/(home)/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="flex border-t justify-between font-medium p-6">
+      <div className="flex items-center gap-2">
+        <p>© 2025 All rights reserved.</p>
+      </div>
+    </footer>
+  );
+}
